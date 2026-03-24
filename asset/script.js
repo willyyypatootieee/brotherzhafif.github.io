28b6666144236c516643c1f0d48ddb7b1b357452
brotherzhafif <bangz1504@gmail.com>
2026-03-24 21:37:42 +0700

    aos threshold adjustmen

--- asset/script.js
+++ asset/script.js
@@ -86,7 +86,7 @@ const scrollObserver = new IntersectionObserver((entries) => {
 }, {
 	root: null,
 	rootMargin: '0px',
-	threshold: 0.1
+	threshold: 0.15
 });
 
 // Fungsi inisialisasi (Panggil ini di tiap render data dinamis!)
